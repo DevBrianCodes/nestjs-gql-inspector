11514250f602650a98d62de00023a1adc0de72c4
Brian Rudolph <brianrudolph@Brians-MacBook-Pro.local>
2025-06-24 10:12:17 +0930

    feat: initial service to handle logs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-export interface GqlInspectorOptions {
-  slowThresholdMs?: number; // Default: 300ms
-  verbose?: boolean; // Log all resolvers if true
-}
 
-export interface ResolverLog {
-  field: string;
-  duration: number;
-  timestamp: string;
-}
